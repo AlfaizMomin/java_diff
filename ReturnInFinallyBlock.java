2da953027f3b75464d4f4df25edf56298ae3fb73
AlfaizMomin <alfaiz.momin1998@gmail.com>
2023-12-11 20:47:24 +0530

    Update ReturnInFinallyBlock.java

--- ReturnInFinallyBlock.java
+++ ReturnInFinallyBlock.java
@@ -2,40 +2,17 @@ public class CodeIssuesExample {
 
     public static void main(String[] args) {
         // Unused local variable issue
-        int unusedVariable;
+        int unusedVariable =true;
 
         // Unconditional if statement issue
         boolean condition = true;
-        if (condition) {
-            // System.out.println("This condition always executes.");
-             // System.out.println("This condition always executes.");
-        }
-
-        // Use of sysout issue
-        System.out.println("This is a sysout statement.");
-
-        // Unused local variable issue
-        int unusedValue;
 
         // Unconditional if statement issue
         boolean anotherCondition = false;
         if (anotherCondition) {
-            System.out.println("This condition always executes as well.");
+            logger.info("This condition always executes as well.");
         }
 
-        // Use of sysout issue
-        System.out.println("Another sysout statement.");
-
-        // Unused local variable issue
-        int someValue;
-
-        // Unconditional if statement issue
-        boolean yetAnotherCondition = true;
-        if (yetAnotherCondition) {
-            System.out.println("Yet another condition that's always true.");
-        }
-
-        // Use of sysout issue
-        System.out.println("One more sysout statement.");
+       
     }
 }
