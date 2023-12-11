4ed0d56d183e057fd4a98a48e269756fc80b8590
AlfaizMomin <alfaiz.momin1998@gmail.com>
2023-12-11 20:43:33 +0530

    Update ReturnInFinallyBlock.java

--- ReturnInFinallyBlock.java
+++ ReturnInFinallyBlock.java
@@ -7,7 +7,7 @@ public class CodeIssuesExample {
         // Unconditional if statement issue
         boolean condition = true;
         if (condition) {
-            System.out.println("This condition always executes.");
+            // System.out.println("This condition always executes.");
              System.out.println("This condition always executes.");
         }
 
